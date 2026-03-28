bdb6f19faaaa85928adb9b01771194793e7e16af
Rohan Ghotekar <rohanghotekar@outlook.com>
2026-03-28 22:16:31 +0530

    second commit

--- src/services/axios.js
+++ src/services/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // All API calls will be prefixed with this automatically.
 // Change this one line when you deploy to AWS.
 const api = axios.create({
-  baseURL: 'http://localhost:1234',
+  baseURL: 'https://13.201.8.73.nip.io',
   headers: {
     'Content-Type': 'application/json' ,
     'ngrok-skip-browser-warning': 'true',
